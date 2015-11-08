980a01091db05aa5af915eab4352e6e157e73548
Shawn Turple <shawn.turple@shaw.ca>
2015-11-08 13:37:36 -0800

    Create Plugin.php

---
+++ Classes/Plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Class PhileLess
+*/
+namespace Phile\Plugin\Sturple\PhileLess;
+class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface {
+	
+	public function __construct() {
+		\Phile\Event::registerEvent('plugins_loaded', $this);
+	}
+	
+	public function on($eventKey, $data = null){
+  
+	}
+}
