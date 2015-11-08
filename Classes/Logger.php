a5b96a82e8c07ec791d9ceea063172be15e1e9d1
Shawn Turple <shawn.turple@shaw.ca>
2015-11-08 14:25:37 -0800

    Create Logger.php

---
+++ Classes/Logger.php
@@ -0,0 +1,10 @@
+<?php
+namespace Phile\Plugin\Sturple\PhileLogger;
+use DateTime;
+use RuntimeException;
+use Psr\Log\AbstractLogger;
+use Psr\Log\LogLevel;
+
+class Logger extends AbstractLogger {
+
+}
