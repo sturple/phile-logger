3ac483425139870a8db418ec5544932f6e5fcc4c
Shawn Turple <shawn.turple@shaw.ca>
2015-11-08 21:13:59 -0800

    Update Plugin.php

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -8,9 +8,13 @@
 *	@link https://github.com/sturple/PhileLogger/
 *	@version 1.0.0
 * 
+* 	@usage
+*   $logger = (new \Phile\Plugin\Sturple\PhileLogger\Plugin($relDir='lib/cache/logs', $logLevel='debug',$options=array()))->getLogger();
+*  	$logger->info('You can use functions debug, info, notice, warning, error, critical, alert.');
+* 	$logger->warning('You can add arrays',array('phile'=>'CMS'));
+* 
 **/
 namespace Phile\Plugin\Sturple\PhileLogger;
-use \Phile\Plugin\Sturple\PhileLogger;
 class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\EventObserverInterface
 {	
 	private $logger = null;
@@ -19,7 +23,7 @@ class Plugin extends \Phile\Plugin\AbstractPlugin implements \Phile\Gateway\Even
 		/* Register Events */
 		\Phile\Event::registerEvent('plugins_loaded', $this);
 		/* create new instance of KLogger */
-		$this->logger = new \Logger(__DIR__ .'/../../../' .   $relDir, $logLevel, $options);				
+		$this->logger = new \Phile\Plugin\Sturple\PhileLogger\Logger(__DIR__ .'/../../../../' .   $relDir, $logLevel, $options);				
 	
 	}
 	public function getLogger()
