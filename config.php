4d687d6036c0d2d854821ff3ecfd95d971c24659
Shawn Turple <shawn.turple@shaw.ca>
2015-11-08 16:38:12 -0800

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,18 @@
  */
 // some default settings for the parser
 return array(
-  'enable' => true,
-  'output' => 'lib/cache/logs/error.log'
+	'active' => false,
+	'relativePath' 		=> 'lib/cache/logs',
+	'logLevel' 			=> 'debug',
+	'options' => array(
+		'extension'      => 'txt',
+		'dateFormat'     => 'Y-m-d G:i:s.u',
+		'filename'       => false,
+		'flushFrequency' => false,
+		'prefix'         => 'log_',
+		'logFormat'      => false,
+		'appendContext'  => true,
+		
+	)
+  
 );
